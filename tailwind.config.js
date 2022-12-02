7cde38d7b78e03c6ee659653bd29222097b0759f
githubotoro <yupuday.git@gmail.com>
2022-12-02 23:12:13 +0530

    daisyui-and-smart-contract-addition

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
+	corePlugins: {
+		container: false,
+	},
 	theme: {
 		extend: {},
+		screens: {
+			sm: { min: "320px", max: "767px" },
+			md: { min: "768px", max: "1023px" },
+			lg: { min: "1024px", max: "1279px" },
+			xl: { min: "1280px", max: "1535px" },
+			xxl: { min: "1536px" },
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
