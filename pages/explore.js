07e0c5cbbd831dacba335772f9dc43264bcab111
githubotoro <yupuday.git@gmail.com>
2022-12-03 14:28:39 +0530

    view-updates

--- pages/explore.js
+++ pages/explore.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Explore() {
-  return <div className="container">Page Content - Explore</div>;
+	return (
+		<>
+			<h1>Explore</h1>
+		</>
+	);
 }
